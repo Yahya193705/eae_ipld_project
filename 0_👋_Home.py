1afc230434310ec3cbde51574890321c4748700b
Yahya <yahyaakd2@gmail.com>
2023-12-20 18:23:39 +0100

    Completed the home page

--- 0_👋_Home.py
+++ 0_👋_Home.py
@@ -39,7 +39,7 @@ st.write(f"""
 
 
 # ----- Personal title or short description -----
-current_role = "Finance & Fintech Graduate / Big Data student"   # TODO: Change this
+current_role = " Big Data & Analytics student"   # TODO: Change this
 
 st.write(f"""<div style="text-align: center;"><h4><i>{current_role}</i></h4></div>""", unsafe_allow_html=True)
 
